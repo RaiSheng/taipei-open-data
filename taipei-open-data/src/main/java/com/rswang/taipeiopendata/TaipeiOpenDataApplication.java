e8a74de4dbd271aa7fa613d92cb70d51f55a5b24
RaiSheng <dryadf16088@gmail.com>
2025-01-06 02:24:03 +0800

    專案初始化

---
+++ taipei-open-data/src/main/java/com/rswang/taipeiopendata/TaipeiOpenDataApplication.java
@@ -0,0 +1,13 @@
+package com.rswang.taipeiopendata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaipeiOpenDataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaipeiOpenDataApplication.class, args);
+    }
+
+}
---
+++ taipei-open-data/src/test/java/com/rswang/taipeiopendata/TaipeiOpenDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rswang.taipeiopendata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaipeiOpenDataApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
